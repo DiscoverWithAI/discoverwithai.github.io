d8a97161e6a2e660e136894fdcdef5cf4e5ed253
IfJay <andreajsm03@gmail.com>
2025-11-08 20:21:03 +0100

    major UI finalized

--- DiscoverWithAI/animations.js
+++ DiscoverWithAI/animations.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    let split = SplitText.create("#thanks", { type: "words, chars" });
+    const btn = document.getElementById("sub-btn");
+    btn.onclick = () => {
+        gsap.to(split.chars, {
+            duration: .3,
+            y: -150,         // animate from 100px below
+            autoAlpha: 1,   // fade in from opacity: 0 and visibility: hidden
+            stagger: 0.03,  // 0.05 seconds between each
+            repeat: 1,
+            repeatDelay: 1.5,
+            yoyo: true,
+        });
+
+    }
 
 });
