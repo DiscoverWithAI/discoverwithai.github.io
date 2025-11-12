7145ca2ce696da1d0ee482cae5dab1bebe282454
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2025-11-12 23:48:14 +0100

    Addedd email submission

--- DiscoverWithAI/js/formSubmission.js
+++ DiscoverWithAI/js/formSubmission.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadCounter()
-    document.getElementById("user-form").onsubmit = () => {
-        submit()
 
+    if(checkSubmitCookie()) {
+        document.getElementById("sub-btn").disabled = true
+    }
+
+    document.getElementById("user-form").onsubmit = (event) => {
+        submit(event)
     }
 
     async function loadCounter() {
@@ -11,11 +15,45 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("counter").innerText = jsonFile.value
     }
 
-    async function submit() {
-        const response = await fetch("https://abacus.jasoncameron.dev/hit/dwai-test-4/submission")
-        const jsonFile = await response.json()
-        document.getElementById("counter").innerText = jsonFile.value
-        window.location.hash = "#form"
+    async function submit(event) {
+        event.preventDefault()
+
+        const email = document.getElementById("input-email").value
+        console.log(email)
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!checkSubmitCookie() && emailPattern.test(email)) {
+            const form = document.getElementById("user-form")
+            const formData = new FormData(form);
+            const email = document.getElementById("input-email").value
+            formData.append("access_key", "5c96cf41-47a2-42c2-8c87-ac3505e28deb");
+            const submitResponse = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            })
+
+            if (submitResponse.ok) {
+                document.cookie = "submit=true"
+                const response = await fetch("https://abacus.jasoncameron.dev/hit/dwai-test-4/submission")
+                const jsonFile = await response.json()
+                document.getElementById("counter").innerText = jsonFile.value
+                document.getElementById("sub-btn").disabled = true
+                window.location.hash = "#form"
+            }
+        }
+
         return false
     }
+
+    function checkSubmitCookie() {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.startsWith('submit=')) {
+            return true;
+            }
+        }
+        return false;
+    }
+
 })
